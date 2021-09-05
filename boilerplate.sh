7cfb0beaae1d2972d32cdfdde4d70ac0c37fb705
luanlsr <luanfswd@gmail.com>
2021-09-05 00:52:11 -0300

    boilerplate fixed

--- boilerplate.sh
+++ boilerplate.sh
@@ -8,133 +8,92 @@ echo "PORT=3000
 DB_URL='mongodb://localhost:27017'
 DB_NAME='databaseName'">>.env
 echo "const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 
 require('dotenv').config();
-const { PORT } = process.env;
-const router = require('./api/routes/routes');
 
-app.get('/', router)
+const { PORT } = process.env;
+const router = require('./routes/router');
+
+app.get('/', router);
 
 app.listen(PORT, () => {
-  console.log(`Acessar: http://localhost:${PORT}`);
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Acessar http://localhost:${PORT}`);
+  console.log(`Online na porta ${PORT}`);
 });
+
 ">>index.js
-npm i express express-rescue express-validations body-parser mongodb dotenv
+npm i express express-rescue express-validations body-parser mongodb dotenv joi
 npm i nodemon -D
 mkdir docs models services controllers helpers validations middlewares routes config
 cd models
 echo "const mongodb = require('mongodb').MongoClient;
 require('dotenv').config();
+
 const { DB_URL, DB_NAME } = process.env;
 
 const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-module.exports = () =>
-    mongodb.connect(DB_URL, OPTIONS)
-    .then((connection) => connection.db(DB_NAME))
-    .catch((err) => {
-        console.log(err);
-        process.exit(1)
-    })">>connection.js
+module.exports = () => mongodb.connect(DB_URL, OPTIONS).then((connection) => connection.db(DB_NAME))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });">>connection.js
 touch model.js
 cd ..
 cd services
-echo "const Author = require('../models/Author');
-const validate = require('../validations/validations')
+echo "const model = require('../models/model');
+const validate = require('../validations/validations');
 
-const getAll = async () => Author.getAll();
+const getAll = async () => model.getAll();
 
 const findById = async (id) => {
-  const author = await Author.findById(id);
+  const author = await model.findById(id);
   if (!author) {
     return {
       error: {
         code: 'notFound',
-        message: `Não foi possível encontrar um autor com o id ${id}`,
+        message: 'Something is wrong',
       },
     };
   }
   return author;
 };
 
-  const create = async (firstName, middleName, lastName) => {
-    const existingAuthor = await Author.findByName(firstName, middleName, lastName);
-  
-    if (existingAuthor) {
-      return {
-        error: {
-          code: 'alreadyExists',
-          message: 'Um autor já existe com esse nome completo',
-        },
-      };
-    }
-    const created = await validate(firstName, middleName, lastName)
-    return Author.create(created);
-  };
+const create = async (firstName, middleName, lastName) => {
+  const existingAuthor = await model.findByName(firstName, middleName, lastName);
+
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Um autor já existe com esse nome completo',
+      },
+    };
+  }
+  const created = await validate(firstName, middleName, lastName);
+  return model.create(created);
+};
 
 module.exports = {
   getAll,
   findById,
   create,
-};">>serviceName.js
+};
+">>serviceName.js
 cd ..
 cd controllers
 touch controllerName.js
 cd ..
 cd validations
-echo "var expressValidations = require('express-validations')
-
-const {
-  isValidFirstname,
-  isValidMiddlename,
-  isValidLastname,
-  isValidFullname,
-  isStrongPassword,
-  isValidDate,
-  isAlpha,
-  isNumeric,
-  isAlphaNumeric,
-  containsNotNumber,
-  containsNotAlphabets,
-  isLength,
-  isValidURL,
-} = expressValidations
-
-const errors_messages = {
-  firstName_invalid: 'first name is not valid',
-  middleName_invalid: 'middle name is not valid',
-  lastName_invalid: 'last name is not valid',
-  firstName_length: 'first name should have length XXX'
-}
-
-const http_status_code = {
-  HTTP_INVALID_STATUS: 422,
-}
-
-const { HTTP_INVALID_STATUS } = http_status_code
-const { firstName_invalid, middleName_invalid, lastName_invalid, firstName_length } = errors_messages
-
-
-const validate = (params) => {
-  switch (true) {
-    case isValidFirstname(firstName): return { code: HTTP_INVALID_STATUS, message: firstName_invalid}
-    case isLength(firstName, 3, null ): return {code: HTTP_INVALID_STATUS, message: firstName_length}
-    case isValidMiddlename(middleName): return { code: HTTP_INVALID_STATUS, message: middleName_invalid}
-    case isValidLastname(lastName): return { code: HTTP_INVALID_STATUS, message: lastName_invalid}
-  
-    default: return {}
-  }
-
-}
-
-module.exports = validate;">>validations.js
+touch validations.js
 cd ..
 cd helpers
 echo "const HTTP_OK_STATUS = 200;
@@ -149,13 +108,21 @@ module.exports = {
   HTTP_NO_BODY_STATUS,
   HTTP_401,
   HTTP_NOT_FOUND_STATUS,
-}">> helpers.js
+};
+">> helpers.js
 cd ..
 cd routes
 echo "const express = require('express');
+
 const router = express.Router();
 
-const { getAll, findById, create, edit, remove } = require('../controllers/controlerName');
+const {
+  getAll,
+  findById,
+  create,
+  edit,
+  remove,
+} = require('../controllers/controllerName');
 
 router.get('/', getAll);
 
@@ -167,8 +134,8 @@ router.put('/', edit);
 
 router.delete('/', remove);
 
-
 module.exports = router;
+
 ">>router.js
 cd ..
 cd middlewares
